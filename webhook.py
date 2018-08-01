2f46e72f860f1d4ce19ad577e4c145db9b1a91ce
deekshayennam <30662792+deekshayennam@users.noreply.github.com>
2018-08-01 11:00:55 -0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -38,7 +38,7 @@ def makeResponse(req):
         if date_formatted==weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
             break
-    speech = "The forecast for"+city+"for "+date_formatted+" is "+condition
+    speech = "The forecast for "+city+" for "+date_formatted+" is "+condition
     return {
     "speech": speech,
     "displayText": speech,
